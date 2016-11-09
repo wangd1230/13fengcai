915ad9c6c986d8893e6f15201829c6b446c2a00c
wangd1230 <790894413@qq.com>
2016-11-09 12:28:13 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('a').click(function(){
+		alert('暂不开放，敬请期待');
+	})
+	// var mySwiper = new Swiper('.swiper-container', {
+	// autoplay:3000,//可选选项，自动滑动
+	// autoplayDisableOnInteraction : false,
+ //    loop:true,
+ //    pagination:'.swiper-pagination',
+ //    paginationClickable :true,
+ //    })
+
+	// $(".lubo").lubo();
+
+	// 		$("a").click(function(){
+	// 			$("html,body").animate({
+	// 				scrollTop:$($(this).attr("href")).offset().top+"px"
+	// 			},{
+	// 				duration:500,
+	// 				easing:"swing"
+	// 			});
+	// 			return false;
+	// 		});		
+	
