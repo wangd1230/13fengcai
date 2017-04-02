0444088b4e360e151ac72b8f97611d5ff256504b
wangd1230 <790894413@qq.com>
2017-04-02 18:41:16 +0800

    d

--- js/main.js
+++ js/main.js
@@ -2,23 +2,5 @@ $(document).ready(function(){
 	$('a').click(function(){
 		alert('暂不开放，敬请期待');
 	})
-	// var mySwiper = new Swiper('.swiper-container', {
-	// autoplay:3000,//可选选项，自动滑动
-	// autoplayDisableOnInteraction : false,
- //    loop:true,
- //    pagination:'.swiper-pagination',
- //    paginationClickable :true,
- //    })
-
-	// $(".lubo").lubo();
-
-	// 		$("a").click(function(){
-	// 			$("html,body").animate({
-	// 				scrollTop:$($(this).attr("href")).offset().top+"px"
-	// 			},{
-	// 				duration:500,
-	// 				easing:"swing"
-	// 			});
-	// 			return false;
-	// 		});		
 	
+})
